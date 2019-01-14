3e03cbeb6f195bcebe1db285b20b523adc3df3a3
Shad Storhaug <shad@shadstorhaug.com>
2019-01-14 22:58:20 +0700

    As suspected, renaming to ps1 caused the browser to stop downloading the file. Reverting that change.

--- RunBoxstarterInstall.bat
+++ RunBoxstarterInstall.bat
@@ -1,1 +1,1 @@
-START http://boxstarter.org/package/url?https://raw.githubusercontent.com/NightOwl888/dev-machine-setup/master/chocolatey-recipe.ps1
+START http://boxstarter.org/package/url?https://raw.githubusercontent.com/NightOwl888/dev-machine-setup/master/chocolatey-recipe.txt
